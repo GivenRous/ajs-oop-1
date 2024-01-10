a9746ddae911f9fa0113e537b813b058267a9fe8
Ivan Surovegin <givenrous@yandex.ru>
2024-01-10 11:29:48 +0300

    initial

---
+++ src/js/character.js
@@ -0,0 +1,17 @@
+export default class Character {
+    constructor(name, type, health, level, attack, defence) {
+      if ((name.length < 2) || (name.length > 10)) {
+        throw new Error('Имя должно содержать от 2 до 10 символов');
+      }
+      const arr = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+      if (!arr.includes(type)) {
+        throw new Error('Недопустимый тип персонажа');
+      }
+      this.name = name;
+      this.type = type;
+      this.health = 100;
+      this.level = 1;
+      this.attack = attack;
+      this.defence = defence;
+    }
+  }
