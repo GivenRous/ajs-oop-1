9a3727b57451ecc32dcf009f3d5257ef9ae3a408
Ivan Surovegin <givenrous@yandex.ru>
2024-01-11 11:30:37 +0300

    правки

--- src/js/bowerman.js
+++ src/js/bowerman.js
@@ -1,7 +1,7 @@
 import Character from "./сharacter";
 
 export default class Bowerman extends Character {
-  constructor(name, type = "Bowman") {
+  constructor(name, type = "Bowerman") {
     super(name, type);
     this.attack = 25;
     this.defence = 25;
